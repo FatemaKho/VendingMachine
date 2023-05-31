d6c824ec563d01c5d9beba026be755c3c5b2e316
Melissa Augustin <melissaaugustin1998@gmail.com>
2023-05-31 17:08:54 -0400

    added changes to view

--- VendingMachine/src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/ui/VendingMachineView.java
@@ -36,3 +36,63 @@ public class VendingMachineView {
         // ...
     }
 }
+
+//import java.math.BigDecimal;
+//import java.util.List;
+//import java.util.Scanner;
+
+//public class VendingMachineView {
+    //private Scanner scanner;
+    
+   // public VendingMachineView() {
+        //scanner = new Scanner(System.in);
+    //}
+    
+    //public void displayItems(List<Item> items) {
+       // System.out.println("Vending Machine Items:");
+       // for (Item item : items) {
+         //   System.out.println(item.getName() + " - $" + item.getCost() + " (Inventory: " + item.getInventoryCount() + ")");
+        //}
+        //System.out.println();
+    //}
+    
+    //public void displayDepositedAmount(BigDecimal depositedAmount) {
+        //System.out.println("Deposited amount: $" + depositedAmount);
+        //System.out.println();
+    //}
+    
+    //public void displayInsufficientFundsMessage() {
+       // System.out.println("Insufficient funds. Please deposit more money.");
+       // System.out.println();
+    //}
+    
+    //public void displayNoItemInventoryMessage() {
+       // System.out.println("Selected item is out of stock. Please choose another item.");
+        //System.out.println();
+    //}
+    
+    //public void displayChangeReturned(Change change) {
+        //System.out.println("Change returned: ");
+        //System.out.println("Quarters: " + change.getQuarters());
+       // System.out.println("Dimes: " + change.getDimes());
+        //System.out.println("Nickels: " + change.getNickels());
+        //System.out.println("Pennies: " + change.getPennies());
+        //System.out.println();
+    //}
+    
+   // public String promptItemSelection() {
+        //System.out.print("Enter the item name: ");
+       // String itemName = scanner.nextLine();
+        //System.out.println();
+        //return itemName;
+    //}
+    
+    //public BigDecimal promptDepositAmount() {
+       // System.out.print("Enter the deposit amount: $");
+       // BigDecimal amount = scanner.nextBigDecimal();
+        //scanner.nextLine(); // Consume the newline character
+        //System.out.println();
+        //return amount;
+    //}
+//}
+//
