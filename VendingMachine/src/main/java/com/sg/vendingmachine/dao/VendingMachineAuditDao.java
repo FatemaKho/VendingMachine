e3677b6c75afceda9482345db58674c5a393f479
strangechick <fkhondker123@gmail.com>
2023-05-31 15:28:45 -0400

    fixed dao interface

--- VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDao.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineAuditDao.java
@@ -1,4 +1,12 @@
 package com.sg.vendingmachine.dao;
 
+import com.sg.vendingmachine.dto.Product;
+import com.sg.vendingmachine.service.VendingMachinePersistenceException;
+
+import java.util.List;
+import java.util.Map;
+
 public interface VendingMachineAuditDao {
+
 }
+
--- VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineDao.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineDao.java
@@ -1,7 +1,13 @@
 package com.sg.vendingmachine.dao;
 
+import com.sg.vendingmachine.dto.Product;
+import com.sg.vendingmachine.service.VendingMachinePersistenceException;
+
+import java.util.List;
+import java.util.Map;
+
 public interface VendingMachineDao {
-  Product addProduct(String productId, Product product);
+    Product addProduct(String productId, Product product);
     List<Product> getAllProducts();
     List<String> getAllProductIds();
     Product getProduct(String productId);
