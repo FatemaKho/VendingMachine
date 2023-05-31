f04ee6724144cfc8a365430be979fa5dd7382e88
Fatema khondker <114109023+FatemaKho@users.noreply.github.com>
2023-05-31 15:30:17 -0400

    Update VendingMachineDao.java

--- VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineDao.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dao/VendingMachineDao.java
@@ -1,4 +1,13 @@
 package com.sg.vendingmachine.dao;
 
 public interface VendingMachineDao {
+  Product addProduct(String productId, Product product);
+    List<Product> getAllProducts();
+    List<String> getAllProductIds();
+    Product getProduct(String productId);
+    Product updateProduct(String productID, Product product);
+    Product removeProduct(String productId);
+    Map<String, Product> loadProductsFromFile() throws VendingMachinePersistenceException;
+    void writeProductsToFile() throws VendingMachinePersistenceException;
+
 }
