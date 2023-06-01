57bbcb2fc8a29ac944bfa0ebea45871b8748348d
Sophia Zhou <102894133+sbagel@users.noreply.github.com>
2023-06-01 09:11:55 -0700

    revert product class for use with productId

--- VendingMachine/src/main/java/com/sg/vendingmachine/dto/Product.java
+++ VendingMachine/src/main/java/com/sg/vendingmachine/dto/Product.java
@@ -4,26 +4,34 @@ import java.math.BigDecimal;
 import java.util.Objects;
 
 public class Product {
+    private String productId;
     private String productName;
     private BigDecimal price;
     private int itemsInStock;
 
     private final String DELIMITER ="::";
 
-//overloaded constructor to initialize
-
-//add in exceptions
-    public Product(String productName, BigDecimal price, int itemsInStock) {
+    public Product(String productId, String productName, BigDecimal price, int itemsInStock) {
+        this.productId = productId;
         this.productName = productName;
         this.price = price;
         this.itemsInStock = itemsInStock;
     }
     public Product(String productAsText){
         String[] fields= productAsText.split(DELIMITER);
-        this.productName=fields[0];
-        this.price=new BigDecimal(fields[1]);
-        this.itemsInStock=Integer.parseInt(fields[2]);//could be null pointer
+        this.productId=fields[0];
+        this.productName=fields[1];
+        this.price=new BigDecimal(fields[2]);
+        this.itemsInStock=Integer.parseInt(fields[3]);//could be null pointer
 
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
     }
 
     public String getProductName() {
@@ -55,13 +63,14 @@ public class Product {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Product product = (Product) o;
-        return itemsInStock == product.itemsInStock && productName.equals(product.productName)  && price.equals(product.price);
+        return itemsInStock == product.itemsInStock && productId.equals(product.productId) && productName.equals(product.productName) && price.equals(product.price);
     }
 
     @Override
     public String toString() {
         return "Product{" +
-                "productName='" + productName + '\'' +
+                "productId='" + productId + '\'' +
+                ", productName='" + productName + '\'' +
                 ", price=" + price +
                 ", itemsInStock=" + itemsInStock +
                 ", DELIMITER='" + DELIMITER + '\'' +
@@ -70,11 +79,11 @@ public class Product {
 
     @Override
     public int hashCode() {
-        return Objects.hash(productName, price, itemsInStock);
+        return Objects.hash(productId, productName, price, itemsInStock);
 
     }
     //check
     public String marshallProductAsText(){
-        return productName+DELIMITER+price;
+        return productId + DELIMITER +productName+DELIMITER+price;
     }
 }
