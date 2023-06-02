8b1053ff9c2e8d69b7594d818748a3c8c63e0221
Lien Nguyen <liennguyen@Liens-MacBook-Pro.local>
2023-06-02 11:59:12 -0400

    tests not passing

--- VendingMachine/src/test/java/VendingMachineDaoImplTest.java
+++ VendingMachine/src/test/java/VendingMachineDaoImplTest.java
@@ -2,7 +2,8 @@ import com.sg.vendingmachine.dao.VendingMachineDao;
 import com.sg.vendingmachine.dao.VendingMachineDaoImpl;
 import com.sg.vendingmachine.dto.Product;
 import com.sg.vendingmachine.service.VendingMachinePersistenceException;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import java.io.FileWriter;
 import java.math.BigDecimal;
@@ -13,16 +14,16 @@ import java.util.TreeMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class VendingMachineDaoImplTest {
-    VendingMachineDao testDao;
 
-public VendingMachineDaoImplTest(){
-    String testFile = "testproducts.txt";
-    VendingMachineDao testDao = new VendingMachineDaoImpl("testproducts.txt");
-}
-    @org.junit.jupiter.api.BeforeEach
-    public void setUp() throws Exception {
-    }
+    public class VendingMachineDaoImplTest {
+        VendingMachineDao testDao;
+
+        @BeforeEach
+        public void setUp() {
+            String testFile = "testproducts.txt";
+            testDao = new VendingMachineDaoImpl(testFile);
+        }
+@org.junit.Test
 @Test
 public void testLoadProductsFromFile () throws Exception {
     System.out.println("loadProductsFromFile");
@@ -30,7 +31,11 @@ public void testLoadProductsFromFile () throws Exception {
     Product p1 = new Product("1", "Chips", bd, 1);
     bd = new BigDecimal("2.50");
     Product p2 = new Product("2", "Water", bd, 1);
-    //Result
+    // Add products to the file before loading
+    testDao.addProduct(Integer.parseInt(p1.getProductId()), p1);
+    testDao.addProduct(Integer.parseInt(p2.getProductId()), p2);
+
+    // Result
     Map<Integer, Product> result = testDao.loadProductsFromFile();
     //Expected Result
     Map<Integer, Product> expectedResult = new TreeMap<>();
@@ -40,6 +45,7 @@ public void testLoadProductsFromFile () throws Exception {
     assertEquals(expectedResult, result, "Test Products");
 }
 
+@org.junit.Test
 @Test
 public void testAddGetProduct() throws VendingMachinePersistenceException {
     System.out.println("addProduct");
@@ -56,29 +62,33 @@ public void testAddGetProduct() throws VendingMachinePersistenceException {
     assertEquals(expectedResult.getItemsInStock(), result.getItemsInStock());
 }
 
-@Test
-public void testGetAllProduct() throws VendingMachinePersistenceException {
-    System.out.println("getAllProducts");
-    BigDecimal bd = new BigDecimal("2.00");
-    Product p1 = new Product("1", "Chips", bd, 1);
-    bd = new BigDecimal ("2.50");
-    Product p2 = new Product("2", "Water", bd, 1);
-    testDao.addProduct(Integer.parseInt(p1.getProductId()),p1);
-    testDao.addProduct(Integer.parseInt(p2.getProductId()),p2);
+        @org.junit.Test
+        @Test
+        public void testGetAllProduct() throws VendingMachinePersistenceException {
+            System.out.println("getAllProducts");
+            BigDecimal bd = new BigDecimal("2.00");
+            Product p1 = new Product("1", "Chips", bd, 1);
+            bd = new BigDecimal("2.50");
+            Product p2 = new Product("2", "Water", bd, 1);
+            testDao.addProduct(Integer.parseInt(p1.getProductId()), p1);
+            testDao.addProduct(Integer.parseInt(p2.getProductId()), p2);
 
-    //Result
-    List<Product> result = testDao.getAllProducts();
-    //ExpectedResult
-    List<Product> expectedResult = new ArrayList<>();
-    expectedResult.add(p1);
-    expectedResult.add(p2);
+            // Result
+            List<Product> result = testDao.getAllProducts();
 
-    //Assert
-    assertNotNull(result, "The list of products must not be null");
-    assertEquals(result.contains(p1), "List of products must equal");
-    assertTrue(result.contains(p1),"The list of products should contain p1");
-    assertTrue(result.contains(p2),"The list of products contain p2");
-    assertEquals(expectedResult, result, "2 lists of products must equal");
+            // Expected Result
+            List<Product> expectedResult = new ArrayList<>();
+            expectedResult.add(p1);
+            expectedResult.add(p2);
+
+            // Assert
+            assertNotNull(result, "The list of products must not be null");
+            assertEquals(expectedResult, result, "The lists of products must be equal");
+            assertTrue(result.contains(p1),"The list of products should contain p1");
+            assertTrue(result.contains(p2),"The list of products contain p2");
+            assertEquals(expectedResult, result, "2 lists of products must equal");
+        }
+
 }
 
-}
+
